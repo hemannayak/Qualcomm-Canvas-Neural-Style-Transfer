77a068f65347925ac20e82086a219d85b6d05197
Pangoth Hemanth Nayak <hemanthnayakpangoth@gmail.com>
2025-10-12 12:42:21 +0530

    commit

--- web_interface.py
+++ web_interface.py
@@ -142,4 +142,5 @@ def output_file(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
